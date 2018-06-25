9f1dd66b8e05db0e5fbd1f4648073423997b1a68
Alec Glassford <glassford@cs.stanford.edu>
2018-06-25 11:56:08 -0700

    Update chatter and labels, color scale, popup

--- src/js/main.js
+++ src/js/main.js
@@ -8,15 +8,22 @@ var L = elements[0].leaflet;
 var maps = elements.map(e => e.map)
 
 // Sync maps
-require('leaflet.sync');
+require("leaflet.sync");
 maps.forEach((map, i) => {
+  var other = maps[(i + 1) % 2];
+
   // Sync map with other
-  map.sync(maps[(i + 1) % 2]);
+  map.sync(other);
+
+  // If popup opens, close popup on other map
+  map.on("popupopen", () => {
+    other.closePopup();
+  });
 
   map.scrollWheelZoom.disable();
 
   // Add labels
-  var topLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png', {
+  var topLayer = L.tileLayer("https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png", {
     opacity: 0.8,
     pane: "markerPane",
   }).addTo(map);
